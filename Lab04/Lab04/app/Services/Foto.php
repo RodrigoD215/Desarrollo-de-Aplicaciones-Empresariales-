e2084f5349d67043841096f95df7ca984c35432a
Rodrigo <RodrigoD215>
2018-11-04 11:22:49 -0500

    Lab 04
    Creacion de data para la BD , validacion de usuarios y implementacion de la funcionalidad para recuperacion de la contraseña.

---
+++ Lab04/Lab04/app/Services/Foto.php
@@ -0,0 +1,34 @@
+<?php namespace GestorImagenes;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Foto extends Model implements AuthenticatableContract, CanResetPasswordContract {
+
+	use Authenticatable, CanResetPassword;
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'fotos';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['id','nombre', 'descripcion', 'ruta','album_id'];
+
+	public function album(){
+		return $this->belongsTo('GestorImagenes\Album');
+	}
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = ['password', 'remember_token'];
+
+}
